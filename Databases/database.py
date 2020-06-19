a813673a6a9102fe99addd57c64bb1f916458dc9
Demeanour <tken.motsoari@gmail.com>
2020-06-19 22:00:37 +0200

    Created a class Databse, it doesn't have any functions yet

--- Databases/database.py
+++ Databases/database.py
@@ -4,4 +4,7 @@ import redis
 ## Database class 
 
 class DB(object):
-    
+    # functions
+    # Create a database connection to the servers
+    # Create a select statement for getting data from the database 
+    # Create a insert statement for adding data to the database## 
