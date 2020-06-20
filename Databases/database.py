6644846c21cdc9fc2e09cd7fe2923f08176002b5
Demeanour <tken.motsoari@gmail.com>
2020-06-20 20:19:30 +0200

    Created the select function in the database class. Still neeeds to be completed and reviewed

--- Databases/database.py
+++ Databases/database.py
@@ -8,3 +8,15 @@ class DB(object):
     # Create a database connection to the servers
     # Create a select statement for getting data from the database 
     # Create a insert statement for adding data to the database## 
+
+    # -- global_variables
+
+    # -- Functions
+    def select(self, user_id, items):
+        """
+         -- Ths function is for selecting items from the couchbase databse
+         -- first it needs to have the userId of the user requesting
+         -- then a json list of items we are looking for.
+
+        """
+        pass
